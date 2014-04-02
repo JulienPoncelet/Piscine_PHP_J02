30b1e2c57730e796f415b218642d4a8d0ad360dd
Julien PONCELET <jponcele@e2r6p2.42.fr>
2014-04-02 11:44:11 +0200

    save via alias

---
+++ ex00/another_world.php
@@ -0,0 +1,36 @@
+#!/usr/bin/php
+<?PHP
+
+function ft_split($str)
+{
+	$split = explode(" ", $str);
+	$len = count($split);
+	$i = 0;
+	while ($i < $len)
+	{
+		if (!$split[$i])
+		{
+			array_splice($split, $i, 1);
+			$len--;
+		}
+		else
+			$i++;
+	}
+	return $split;
+}
+
+if ($argc > 1)
+{
+	$tmp = preg_replace("/\t/", "/ /", $argv[1]);
+	$tmp = ft_split($tmp);
+	foreach ($tmp as $key => $value)
+	{
+		if ($key)
+			print(" ".$value);
+		else
+			print($value);
+	}
+	print("\n");
+}
+
+?>
